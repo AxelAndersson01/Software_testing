92f143f56558c5d6304e24d7b26645000346e454
Axel Andersson <axan21@student.bth.se>
2024-05-27 15:29:50 +0200

    updated test and runs

--- test_suite_pickle.py
+++ test_suite_pickle.py
@@ -21,7 +21,10 @@ test_cases = {
     "set": {1, 2, 3},
     "nested": {"a": [1, 2, 3], "b": {"c": 3.14}},
     "custom_obj": type("TestClass", (object,), {"attr": 42})(),
-    "recursive_list": lambda: None
+    "recursive_list": lambda: None,
+    "random_string": "\n",
+    "bytes": bytes(15),
+    "complex": complex(1j)
 }
 
 # Create recursive data structure
